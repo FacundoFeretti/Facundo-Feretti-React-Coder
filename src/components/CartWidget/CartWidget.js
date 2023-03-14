5b925dbfc7d508ebdb0ba52d10e290ae184af340
Facundo Feretti <facundoferetti@gmail.com>
2023-03-14 00:03:14 -0300

    Vuelvo a cargar archivos por falla con git

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import "./CartWidget.css"
+
+export const CartWidget = () => {
+    return (
+        <li id="cartElement"><div id="cartCount">2</div><i class="fa-solid fa-cart-shopping"></i></li>
+    )
+}
+
+
+
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import "./NavBar.css"
+import "../CartWidget/CartWidget"
+import { CartWidget } from "../CartWidget/CartWidget"
+
+export const NavBar = (props) => {
+    console.log(props)
+    
+    return (
+        <nav id="navbar">
+            <a id="title">FacuTech</a>
+            <ul id="navMenu">
+                <CartWidget />
+                <li className="itemMenu"><a href="#">Productos</a></li>
+                <li className="itemMenu"><a href="#">Sobre Nosotros</a></li>
+                <li className="itemMenu"><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
